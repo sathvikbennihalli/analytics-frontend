ff4b33e00b0b2c7e590e094af9b20e345d9b6c7f
“sathvikbennihalli” <sathwikrb21@gmail.com>
2024-05-31 16:23:40 +0530

    table design updates

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -8,15 +8,6 @@ const FileUpload = () => {
   const [data, setData] = useState([]);
   const [message, setMessage] = useState("");
   const [tableName, setTableName] = useState("");
-  // eslint-disable-next-line no-unused-vars
-  const [orderCounts, setOrderCounts] = useState({
-    "0-10": 0,
-    "10-30": 0,
-    "30-60": 0,
-    ">60": 0,
-  });
-  // eslint-disable-next-line no-unused-vars
-  const [totalUsers, setTotalUsers] = useState(0);
 
   const handleFileChange = (e) => {
     const selectedFile = e.target.files[0];
@@ -32,57 +23,13 @@ const FileUpload = () => {
       const sheetName = workbook.SheetNames[0]; // Assuming the first sheet
       const sheet = workbook.Sheets[sheetName];
       const jsonData = XLSX.utils.sheet_to_json(sheet, {
-        raw: false, // Ensure that dates and times are parsed as actual Date objects
+        raw: false,
       });
 
-      // Convert date and time columns from numeric to Date objects
-      const convertedData = jsonData.map((row) => {
-        // Iterate over each property in the row
-        for (const key in row) {
-          // Check if the value is a numeric date (Excel dates are usually above 40000)
-          if (typeof row[key] === "number" && row[key] > 25567) {
-            row[key] = new Date((row[key] - (25567 + 1)) * 86400 * 1000);
-          }
-          // Check if the value is a numeric time (fraction of a day)
-          else if (typeof row[key] === "number" && row[key] <= 1) {
-            const hours = Math.floor(row[key] * 24);
-            const minutes = Math.round(((row[key] * 24) % 1) * 60);
-            row[key] = new Date(1970, 0, 1, hours, minutes);
-          }
-        }
-        return row;
-      });
-
-      setData(convertedData);
-      localStorage.removeItem("totalUsers");
-      localStorage.removeItem("orderCounts");
-      localStorage.removeItem("tableName");
-      calculateOrderCounts(convertedData);
-      setTotalUsers(convertedData.length);
-      localStorage.setItem("uploadedData", JSON.stringify(convertedData));
+      setData(jsonData);
+      localStorage.setItem("uploadedData", JSON.stringify(jsonData));
     };
     reader.readAsBinaryString(file);
-  };
-
-  const calculateOrderCounts = (data) => {
-    const counts = {
-      "0-10": 0,
-      "10-30": 0,
-      "30-60": 0,
-      ">60": 0,
-    };
-
-    data.forEach((row) => {
-      const orderCount = parseInt(row["30 Days Order Count"], 10);
-      if (orderCount >= 0 && orderCount <= 10) counts["0-10"]++;
-      if (orderCount > 10 && orderCount <= 30) counts["10-30"]++;
-      if (orderCount > 30 && orderCount <= 60) counts["30-60"]++;
-      if (orderCount > 60) counts[">60"]++;
-    });
-
-    setOrderCounts(counts);
-    localStorage.setItem("orderCounts", JSON.stringify(counts));
-    localStorage.setItem("totalUsers", JSON.stringify(data.length));
   };
 
   const handleSubmit = async (e) => {
